c8eb1d810e5067dc3590db4e34a6a91207d294a6
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2025-06-23 11:33:28 +0200

    Remove unused debugging utility methods `NotNull` and `Null` from `Assume` class in `LasseVK.Extensions.Core`.

--- src/LasseVK.Extensions.Core/Assume.cs
+++ src/LasseVK.Extensions.Core/Assume.cs
@@ -14,32 +14,4 @@ public static class Assume
             Debug.WriteLine($"Assumption failed: {expression} in {memberName} at {sourceFilePath}:{sourceLineNumber}");
         }
     }
-
-    [Conditional("DEBUG")]
-    public static void NotNull<T>(
-        [NotNull] T? value,
-        [CallerArgumentExpression(nameof(value))] string? expression = null,
-        [CallerMemberName] string? memberName = null,
-        [CallerFilePath] string? sourceFilePath = null,
-        [CallerLineNumber] int sourceLineNumber = 0)
-    {
-        if (value is null)
-        {
-            Debug.WriteLine($"Assumption failed: {expression} was null in {memberName} at {sourceFilePath}:{sourceLineNumber}");
-        }
-    }
-
-    [Conditional("DEBUG")]
-    public static void Null<T>(
-        [MaybeNull] T? value,
-        [CallerArgumentExpression(nameof(value))] string? expression = null,
-        [CallerMemberName] string? memberName = null,
-        [CallerFilePath] string? sourceFilePath = null,
-        [CallerLineNumber] int sourceLineNumber = 0)
-    {
-        if (value is not null)
-        {
-            Debug.WriteLine($"Assumption failed: {expression} was not null in {memberName} at {sourceFilePath}:{sourceLineNumber}");
-        }
-    }
 }
